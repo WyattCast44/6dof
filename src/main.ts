046c7f9ea40502ddf43f261c890e9381ef809e41
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2025-06-23 21:55:03 -0700

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import Environment from "./support/enviroment/Enviroment";
-import ConstantGravityModel from "./support/gravity/ConstantGravityModel";
-import StandardAtmosphere1976 from "./support/atmosphere/StandardAtmosphere1976";
-import WindModel from "./support/wind/WindModel";
 import Wind from "./support/wind/Wind";
-import CardinalDegree from "./support/angles/CardinalDegree";
-import Knots from "./support/velocity/Knots";
-import AircraftDynamicsModel from "./support/aircraft/AircraftDynamicsModel";
-import Integrator from "./support/numerical/Integrator";
-import StateVector from "./support/numerical/StateVector";
-import LightFixedWing from "./support/aircraft/LightFixedWing";
-import MetersPerSecond from "./support/velocity/MetersPerSecond";
-import Meters from "./support/length/Meters";
-import EulerAngles from "./support/attitude/EulerAngles";
-import RotationalVelocities from "./support/attitude/RotationalVelocities";
 import MSL from "./support/altitude/MSL";
 import Feet from "./support/length/Feet";
+import Knots from "./support/velocity/Knots";
+import Meters from "./support/length/Meters";
+import Seconds from "./support/time/Seconds";
+import WindModel from "./support/wind/WindModel";
+import Simulation from "./support/sim/Simulation";
+import Integrator from "./support/numerical/Integrator";
+import EulerAngles from "./support/attitude/EulerAngles";
+import Environment from "./support/enviroment/Enviroment";
+import StateVector from "./support/numerical/StateVector";
+import CardinalDegree from "./support/angles/CardinalDegree";
 import PositionVector from "./support/vectors/PositionVector";
 import VelocityVector from "./support/vectors/VelocityVector";
-import Simulation from "./support/sim/Simulation";
-import Seconds from "./support/time/Seconds";
+import LightFixedWing from "./support/aircraft/LightFixedWing";
+import MetersPerSecond from "./support/velocity/MetersPerSecond";
+import ConstantGravityModel from "./support/gravity/ConstantGravityModel";
+import RotationalVelocities from "./support/attitude/RotationalVelocities";
+import AircraftDynamicsModel from "./support/aircraft/AircraftDynamicsModel";
+import StandardAtmosphere1976 from "./support/atmosphere/StandardAtmosphere1976";
+import RadiansPerSecond from "./support/rates/RadiansPerSecond";
 
 /**
  * Build the wind model
@@ -52,12 +53,12 @@ let aircraft = new LightFixedWing();
 let initialAircraftState = new StateVector(
   new PositionVector(new Meters(0), new Meters(0), new Meters(-1000)), // start at 0, 0, 1000 m above sea level
   new VelocityVector(
-    new MetersPerSecond(50),
+    new MetersPerSecond(0),
     new MetersPerSecond(0),
     new MetersPerSecond(0)
   ), // start at 50 m/s forward, 0 lateral and vertical velocity
   new EulerAngles(0, 0, 0), // start at 0, 0, 0 euler angles
-  new RotationalVelocities(0, 0, 0) // start at 0, 0, 0 rotational velocities
+  new RotationalVelocities(new RadiansPerSecond(0.5), 0, 0) // start at 0, 0, 0 rotational velocities
 );
 
 let aircraftDynamicsModel = new AircraftDynamicsModel(aircraft, environment);
--- src/support/attitude/EulerAngles.ts
+++ src/support/attitude/EulerAngles.ts
@@ -11,13 +11,13 @@ class EulerAngles {
     bank_phi: Radians|number
   ) {
     this.azimuth_psi = azimuth_psi instanceof Radians ? azimuth_psi : new Radians(azimuth_psi);
-    this.validateAzimuth(this.azimuth_psi);
+    //this.validateAzimuth(this.azimuth_psi);
 
     this.elevation_theta = elevation_theta instanceof Radians ? elevation_theta : new Radians(elevation_theta);
-    this.validateElevation(this.elevation_theta);
+    //this.validateElevation(this.elevation_theta);
 
     this.bank_phi = bank_phi instanceof Radians ? bank_phi : new Radians(bank_phi);
-    this.validateBank(this.bank_phi);
+    //this.validateBank(this.bank_phi);
   }
 
   private validateAzimuth(azimuth: Radians) {
